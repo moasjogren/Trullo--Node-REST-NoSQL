9594d426de8d2fe013375a2a9bbfe12ff5a38935
Moa Sjögren <moasjgren@gmail.com>
2025-10-23 10:56:25 +0200

    fix for deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,12 @@ import express from "express";
 import cors from "cors";
 
 import { connectDB } from "./config/db";
-import { PORT } from "./config/variables";
 
 import userRoutes from "./routes/userRoutes";
 import taskRoutes from "./routes/taskRoutes";
 
 const app = express();
+const PORT: any = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -21,7 +21,7 @@ connectDB()
       res.send("Hello from server");
     });
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
